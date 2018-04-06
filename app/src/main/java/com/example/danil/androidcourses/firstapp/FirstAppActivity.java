13bcf081ebf395bd62ba52e27203bc1a1dc64fa6
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-04-06 23:11:03 +0300

    rename id of button

--- app/src/main/java/com/example/danil/androidcourses/firstapp/FirstAppActivity.java
+++ app/src/main/java/com/example/danil/androidcourses/firstapp/FirstAppActivity.java
@@ -18,7 +18,7 @@ public class FirstAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_app);
         textChanget = (TextView)findViewById(R.id.text_changed_text);
-        Button btnChange1 = (Button) findViewById(R.id.btn_change_1);
+        Button btnChange1 = (Button) findViewById(R.id.btn_change_text1);
 
         btnChange1.setOnClickListener(new View.OnClickListener() {
             @Override
